8a0964a5bc61d602ba7a122d71896a722e64edfb
DFloor Danny <37215615+dan-homoncik@users.noreply.github.com>
2018-08-25 16:45:18 -0400

    uploaded base friends data file

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+	{
+		name: "Richard Hendricks",
+		photo: "",
+		scores: [5, 1, 2, 3, 4, 2, 5, 2, 3, 1]
+	},
+	{
+		name: "Erlich Bachmann",
+		photo: "",
+		scores: [2, 5, 4, 1, 2, 3, 3, 5, 5, 3]
+	},
+	{
+		name: "Jian Yang",
+		photo: "",
+		scores: [1, 1, 5, 5, 3, 5, 1, 2, 1, 1]
+	},
+	{
+		name: "Jared Dunn",
+		photo: "",
+        scores: [3, 1, 3, 5, 2, 3, 4, 1, 5, 5]
+        
+	},
+	{
+		name: "Gavin Belson",
+		photo: "",
+		scores: [3, 1, 5, 2, 2, 4, 3, 5, 1, 3]
+    },
+    {
+		name: "Dinesh Chugtai",
+		photo: "",
+		scores: [5, 3, 4, 3, 2, 2, 5, 1, 1, 2]
+    },
+    {
+		name: "Betram Guilfoyle",
+		photo: "",
+		scores: [1, 4, 5, 2, 1, 1, 3, 1, 4, 3]
+	}
+];
+
+module.exports = friends;
