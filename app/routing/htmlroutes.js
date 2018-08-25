0b2815798649fe9d87cc13fd7c70b95eaa32e819
DFloor Danny <37215615+dan-homoncik@users.noreply.github.com>
2018-08-25 16:51:39 -0400

    small chnage to html routing file

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,12 +1,12 @@
 var path = require("path");
 
 module.exports = function(app) {
-	// if user enters survey in URL or presses survey button, serves the survey HTML file
+	// survey route
 	app.get("/survey", function(req, res) {
 		res.sendFile(path.join(__dirname, "/../public/survey.html"));
 	});
 
-	// fallback use route for homepage
+	// default
 	app.use(function(req, res) {
 		res.sendFile(path.join(__dirname, "/../public/home.html"));
 	});
